968ab068129a08722c7907ed7d091593df6055c7
joshua-fernandess <fernandessjosh@gmail.com>
2024-08-06 17:19:50 -0300

    testando

--- atualizar.js
+++ atualizar.js
@@ -3,7 +3,7 @@ let usuarios = require('./usuarios.js');
 function atualizarUsuario(){
     const index = usuarios.findIndex(usuario => usuario.id === id);
 
-    if(index !== 1){
+    if(index !== -1){
         usuarios[index] = {id, ...novoUsuario};
     }
 }
